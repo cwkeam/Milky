3573147f7c65154b99691ddb1363b98632f183c3
Chan Woo Kim <chanwookim@Chans-MacBook-Pro.local>
2017-08-07 23:48:54 +0800

    responsive

--- public/jq.js
+++ public/jq.js
@@ -15,21 +15,51 @@ $('.tars').click(function(){
 	$(".main").moveDown();
 });
 
-// if(clickedLearnMore){
-	$(".main").onepage_scroll({
-	   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
-	   easing: "cubic-bezier(0.93, 0.04, 1, 1)",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
-	                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-	   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
-	   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
-	   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-	   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
-	   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
-	   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-	   keyboard: true,                  // You can activate the keyboard controls
-	   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
-	                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
-	                                    // the browser's width is less than 600, the fallback will kick in.
-	   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
-	});
-// }
+$(".main").onepage_scroll({
+   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+   easing: "cubic-bezier(0.93, 0.04, 1, 1)",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+   keyboard: true,                  // You can activate the keyboard controls
+   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                    // the browser's width is less than 600, the fallback will kick in.
+   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+});
+
+$(document).ready(function(){
+	if($(window).height() > $(window).width()){
+		$('.stars').css('background-image','url("/public/starryvertical.png")');
+		$('h1.heading').css('padding','0 10%');
+		$('ul.og').css('margin-top', '23vh');
+	}else if(($(window).width()-$(window).height())<100){
+		$('ul.og').css('margin-top', '27vh');
+	}else if(($(window).width()-$(window).height())<150){
+		$('ul.og').css('margin-top', '20vh');
+	}else{
+		$('.stars').css('background-image', 'url("/public/starrybackground.png")');
+		$('h1.heading').css('padding','0 30%');
+		$('ul.og').css('margin-top', '30vh');
+	}
+});
+$(window).resize(function(){
+	if($(window).height() > $(window).width()){
+		$('.stars').css('background-image','url("/public/starryvertical.png")');
+		$('h1.heading').css('padding','0 15%');
+		$('ul.og').css('margin-top', '18vh');
+	}else if(($(window).width()-$(window).height())<100){
+		$('ul.og').css('margin-top', '27vh');
+	}else if(($(window).width()-$(window).height())<100){
+		$('ul.og').css('margin-top', '27vh');
+	}else{
+		$('.stars').css('background-image', 'url("/public/starrybackground.png")');
+		$('h1.heading').css('padding','0 30%');
+		$('ul.og').css('margin-top', '30vh');
+	}
+	
+});
